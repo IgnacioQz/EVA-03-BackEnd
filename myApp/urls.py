774197723e6e1f856e52ff2d23b0f03493b531a7
IgnacioQz <nacho.quierozepeda@gmail.com>
2025-11-17 18:32:06 -0300

    Templates y urls basicas creadas

--- myApp/urls.py
+++ myApp/urls.py
@@ -1,6 +1,10 @@
-from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.welcome, name='welcome'),
+    path('login/', views.login_view, name='login'),
+    path('main/', views.main, name='main'),
+    path('admin-reservas/', views.admin_view, name='admin'),
 ]
+
--- myApp/views.py
+++ myApp/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def welcome(request):
+    return render(request, 'welcome.html')
+
+def redirect_gruop(user):
+    if user.is_superuser:
+        return redirect('admin')
+    else:
+        return redirect('main')
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username", "").strip()
+        password = request.POST.get("password", "").strip()
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect_gruop(user)
+        messages.error(request, "Usuario o contraseña incorrectos.")
+    return render(request, "login.html")
+
+def logout_view(request):
+    """Cierra sesión y vuelve a welcome."""
+    auth_logout(request)
+    return redirect(request, "welcome")
+
+@login_required
+def main(request):
+    return render(request, "main.html")
+
+@login_required
+def admin_view(request):
+    if not request.user.is_superuser:
+        messages.error(request, "No tienes permisos para acceder a esta página.")
+        return redirect("welcome")
+    return render(request,'admin_dashboard.html') 
