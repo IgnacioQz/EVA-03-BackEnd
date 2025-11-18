a968b2fd00bb4e3c0c8c54cb5879eb63ee21af5a
IgnacioQz <nacho.quierozepeda@gmail.com>
2025-11-18 20:00:28 -0300

    Templeates creados

--- myApp/urls.py
+++ myApp/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
-    path('formulario/', views.formulario, name="formulario")
+    path('formulario/', views.formulario, name="formulario"),
+    path('sala/<int:id>/', views.detalleSala, name='sala_detalle'),
+
 ]
 
--- myApp/views.py
+++ myApp/views.py
@@ -5,16 +5,22 @@ from .models import Reserva,Sala
 
 def main(request):
     Sala.limpiar_reservas_vencidas()
-    return render(request, "main.html")
+    salas = Sala.objects.all()
+    reservas = Reserva.objects.all()
+    context = {}
+
+    context['salas'] = salas
+    context['reservas'] = reservas
+
+    return render(request, "main.html", context)
 
 
 def formulario(request):
+    context = {}
     eliminadas = Sala.limpiar_reservas_vencidas()
 
     if eliminadas > 0:
         messages.success(request, f"Se limpiaron {eliminadas} reserva(s) vencida(s)")
-
-    context = {}
     
     if request.method == "POST":
         form = ReservaForm(request.POST)
@@ -25,6 +31,14 @@ def formulario(request):
         else:
             context['form'] = form  # Mantenemos el formulario con los datos ingresados
     else:
-        context['form'] = ReservaForm()  #
+        context['form'] = ReservaForm()  # Creamos nuevo formulario
     
     return render(request, "formulario.html", context)
+
+def detalleSala(request,id):
+    context = {}
+    sala = Sala.objects.get(id = id)  
+    reservas = Reserva.objects.all()  
+    context["reservas"] = reservas
+    context["sala"] = sala
+    return render(request,"detalle.html", context)
