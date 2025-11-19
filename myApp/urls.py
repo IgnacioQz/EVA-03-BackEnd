10d536274d44eaa09b6dc8106377da84e222dd77
IgnacioQz <nacho.quierozepeda@gmail.com>
2025-11-18 23:38:15 -0300

    Formato rut correjido

--- myApp/urls.py
+++ myApp/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('', views.main, name='main'),
     path('formulario/', views.formulario, name="formulario"),
     path('sala/<int:id>/', views.detalleSala, name='sala_detalle'),
-
 ]
 
